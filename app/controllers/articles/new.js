bbace232937202c11dcc235722ef4d44342f09f9
jdetch <jodidetch@gmail.com>
2015-04-24 12:28:52 -0400

    Fix import statement

--- app/controllers/articles/new.js
+++ app/controllers/articles/new.js
@@ -1,3 +1,5 @@
+import Ember from 'ember'
+
 export default Ember.Controller.extend({
   hasDescription: Ember.computed.notEmpty('model.description'),
   isValid: Ember.computed.and('hasDescription'),
