69e2ed6f03f43782b91e2db1cd3fb9ae0b396f70
jdetch <jodidetch@gmail.com>
2015-05-01 16:10:51 -0400

    Use deactivate on routes to clear out unsaved or unchanged records

--- app/routes/articles/index.js
+++ app/routes/articles/index.js
@@ -4,6 +4,12 @@ export default Ember.Route.extend({
   model: function() {
     return this.modelFor('friends/show').get('articles');
   },
+
+  deactivate: function() {
+    var model = this.modelFor('articles/new');
+    model.destroyRecord();
+  },
+
   actions: {
     save: function(model) {
       model.save();
